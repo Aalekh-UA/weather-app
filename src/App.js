9e26b9f44113508c47218add231fc7654afa6274
Aalekh <66673876+Aalekh-UA@users.noreply.github.com>
2022-01-16 13:16:15 +0530

    added location api and weather api modifications

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { GET_WEATHER_USING_LOCATION } from './networkingModule/api';
+import { API_KEY } from './networkingModule/secrets';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  componentDidMount() {
+    this.checkAndGetGeoLocation();
+  }
+
+  checkAndGetGeoLocation = () => {
+    //first check if laptop supports geolocation
+    if (navigator.geolocation) {
+      //get the geolocation
+      navigator.geolocation.getCurrentPosition((position) => {
+        //nullability check
+        if (position) {
+          const lat = position.coords.latitude;
+          const lon = position.coords.longitude;
+          this.getWeatherUsingLocationParams(lat, lon);
+        }
+      });
+    } else {
+      //do error case handling
+      console.log('location feature unavailable');
+    }
+  };
+
+  getWeatherUsingLocationParams = async (lat, lon) => {
+    //fetch data based on lat and long provided
+    const data = await fetch(
+      `${GET_WEATHER_USING_LOCATION}weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`
+    );
+    //convert the readable stream to json
+    const jsData = await data.json();
+    //consume json
+    console.log(jsData);
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello world</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
